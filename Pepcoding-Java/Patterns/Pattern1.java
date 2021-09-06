01ccab837802c5af4132a9a7e67e8b756d6f6402
HarshShiyani <56556849+HarshShiyani@users.noreply.github.com>
2021-09-06 13:26:18 +0530

    Create Pattern1.java

---
+++ Pepcoding-Java/Patterns/Pattern1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Pattern1 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=1; j++){
+                System.out.println("*\t");
+            }
+        }
+
+    }
+}
