762dca9c878d7c612fdce248fa1ae1362685a617
HarshShiyani <56556849+HarshShiyani@users.noreply.github.com>
2021-09-25 22:37:16 +0530

    Create MaxOfAnArray.java

---
+++ Pepcoding-Java/Recursion/MaxOfAnArray.java
@@ -0,0 +1,26 @@
+https://www.pepcoding.com/resources/online-java-foundation/recursion-in-arrays/max-of-an-array-official/ojquestion
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++)
+            arr[i] = sc.nextInt();
+        
+        System.out.println(maxOfArray(arr,0));
+    }
+
+    public static int maxOfArray(int[] arr, int idx){
+        
+        if(idx == arr.length-1)
+            return arr[idx];
+        
+        return Math.max(arr[idx], maxOfArray(arr, idx+1));
+    }
+
+}
